c3758fae5538e9a526131e0315d2e42550e5e690
Taeshawit Suwanwigo <s.taeshawit@gmail.com>
2019-03-27 00:15:39 +0700

    wiring up the provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
   return [
     { title: 'Hello My Love', duration: '3:29' },
@@ -13,3 +15,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+})
